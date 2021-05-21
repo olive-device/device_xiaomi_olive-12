797a7a5d835f7b40f8b78a99e30ff132b631b3c8
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-05-21 09:48:29 +0800

    olive: biometrics: sepolicy: Import from sdm439-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,6 +172,7 @@ VENDOR_SECURITY_PATCH := 2021-07-05
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/biometrics/sepolicy
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Treble
